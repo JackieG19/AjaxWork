98e97b0f8de6a48d192b61086e487d6ec87d1b60
Jackie <jacgut19@gmail.com>
2019-02-07 17:11:26 +0000

    grabbing the name of a user through the php file linked to function in the html file

--- AjaxPHP/proccess.php
+++ AjaxPHP/proccess.php
@@ -1,2 +1,13 @@
 <?php
 echo 'processing...';
+
+// check for GET variable
+
+// if(is set(super global[search a variable called name]))
+if(isset($_GET['name'])){
+    
+    // if it exist
+    echo 'GET: Your name is '.$_GET['name'];
+}
+
+// NOTE: this is pretty unsafe but this is just to show how it works
