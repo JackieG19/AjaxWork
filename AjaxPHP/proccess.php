decae2225acb0fe3e585c661cbfce89ed8f373c2
Jackie <jacgut19@gmail.com>
2019-02-09 18:25:12 +0000

    creating a phpmyadmin database and adding users into the database

--- AjaxPHP/proccess.php
+++ AjaxPHP/proccess.php
@@ -1,14 +1,24 @@
 <?php
+
+// connect to database
+$conn = mysqli_connect('localhost', 'jackiegut19', '', 'ajaxtest', 3306);
+
 echo 'processing...';
 
-// check for POST variable
 if(isset($_POST['name'])){
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
     echo 'POST: Your name is '.$_POST['name'];
-} // its redirecting to the actual php page and its output a new post name
+    
+    $query = "INSERT INTO users(name) VALUE('$name')";
+    
+    if(mysqli_query($conn, $query)){
+        echo 'User Added...';
+    } else {
+        echo 'ERROR: ' . mysqli_error($conn);
+    }
+}
 
 // check for GET variable
 if(isset($_GET['name'])){
     echo 'GET: Your name is '.$_GET['name'];
 }
-
-// NOTE: this is pretty unsafe but this is just to show how it works
